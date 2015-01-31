275c602b06612aaea6376d41a7061aa1f030220d
Eetu Pitkänen <pitkanen.eetu@gmail.com>
2015-01-31 11:50:48 +0200

    GameManager creation

---
+++ LeafPotu/Classes/GameManager.cpp
@@ -0,0 +1,3 @@
+#include "GameManager.h"
+
+using namespace cocos2d;
---
+++ LeafPotu/Classes/GameManager.h
@@ -0,0 +1,17 @@
+#ifndef GAMEMANAGER
+#define GAMEMANAGER
+
+#include "standards.h"
+#include "cocos2d.h"
+
+class GameManager
+{
+public:
+	void EndGame();
+	void PauseGame();
+	int Score;
+
+private:
+
+};
+#endif
