ffdd4adfd79139060ec6337d6913d480b6f04613
Eetu Pitkänen <pitkanen.eetu@gmail.com>
2015-01-31 12:43:24 +0200

    GameManager basic functions added

--- LeafPotu/Classes/GameManager.cpp
+++ LeafPotu/Classes/GameManager.cpp
@@ -1,3 +1,35 @@
 #include "GameManager.h"
 
 using namespace cocos2d;
+
+void GameManager::init(cocos2d::Layer* layer, TileableWorld* world, b2Body* Body)
+{
+	gameLayer = layer;
+	tileableWorld = world;
+	boxBody = Body;
+}
+
+void GameManager::InputCoordinates(Vec2 Coordinates)
+{
+	
+}
+
+void GameManager::PauseGame()
+{
+	if (Paused == false)
+	{
+		Paused = true;
+		// Pause Game
+	}
+	else
+	{
+		Paused = false;
+		// Un-pause Game
+	}
+}
+
+void GameManager::EndGame()
+{
+
+}
+
