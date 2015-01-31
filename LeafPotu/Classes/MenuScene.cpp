5f2c344a2cfef1568d013d7762210e843373a0f2
Eetu Pitkänen <pitkanen.eetu@gmail.com>
2015-01-31 17:14:02 +0200

    Main Menu button positions

--- LeafPotu/Classes/MenuScene.cpp
+++ LeafPotu/Classes/MenuScene.cpp
@@ -28,59 +28,56 @@ bool MenuScene::init()
 	Node* uiNode = CSLoader::createNode("MenuScene.csb");
 	this->addChild(uiNode);
 
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	auto windowSize = Director::getInstance()->getWinSize();
+
+
 	ui::Button* playButton = (ui::Button*)uiNode->getChildByName("BTN_play");
 	playButton->addTouchEventListener(this, toucheventselector(MenuScene::PlayGame));
-	uiNode->addChild(playButton);
 
 	ui::Button* muteButton = (ui::Button*)uiNode->getChildByName("BTN_mute");
 	muteButton->addTouchEventListener(this, toucheventselector(MenuScene::MuteGame));
-	uiNode->addChild(muteButton);
+	muteButton->setPosition(Vec2(muteButton->getPosition().x, visibleSize.height - ((windowSize.height - visibleSize.height) / 2)));
 
 	ui::Button* exitButton = (ui::Button*)uiNode->getChildByName("BTN_exit");
 	exitButton->addTouchEventListener(this, toucheventselector(MenuScene::ExitGame));
-	uiNode->addChild(exitButton);
+	exitButton->setPosition(Vec2(exitButton->getPosition().x, visibleSize.height - ((windowSize.height - visibleSize.height) / 2)));
 
 	ui::Text* labelTop1Name = (ui::Text*)uiNode->getChildByName("LABEL_top1name");
 	labelTop1Name->setText("Hello Cocos!");
 	labelTop1Name->setFontName("Marker Felt");
 	labelTop1Name->setFontSize(30);
 	labelTop1Name->setColor(Color3B(159, 168, 176));
-	uiNode->addChild(labelTop1Name);
 
 	ui::Text* labelTop2Name = (ui::Text*)uiNode->getChildByName("LABEL_top2name");
 	labelTop2Name->setText("Hello Cocos!");
 	labelTop2Name->setFontName("Marker Felt");
 	labelTop2Name->setFontSize(30);
 	labelTop2Name->setColor(Color3B(159, 168, 176));
-	uiNode->addChild(labelTop2Name);
 
 	ui::Text* labelTop3Name = (ui::Text*)uiNode->getChildByName("LABEL_top3name");
 	labelTop3Name->setText("Hello Cocos!");
 	labelTop3Name->setFontName("Marker Felt");
 	labelTop3Name->setFontSize(30);
 	labelTop3Name->setColor(Color3B(159, 168, 176));
-	uiNode->addChild(labelTop3Name);
 
 	ui::Text* labelTop1Score = (ui::Text*)uiNode->getChildByName("LABEL_top1score");
 	labelTop1Score->setText("Hello Cocos!");
 	labelTop1Score->setFontName("Marker Felt");
 	labelTop1Score->setFontSize(30);
 	labelTop1Score->setColor(Color3B(159, 168, 176));
-	uiNode->addChild(labelTop1Score);
 
 	ui::Text* labelTop2Score = (ui::Text*)uiNode->getChildByName("LABEL_top2score");
 	labelTop2Score->setText("Hello Cocos!");
 	labelTop2Score->setFontName("Marker Felt");
 	labelTop2Score->setFontSize(30);
 	labelTop2Score->setColor(Color3B(159, 168, 176));
-	uiNode->addChild(labelTop2Score);
 
 	ui::Text* labelTop3Score = (ui::Text*)uiNode->getChildByName("LABEL_top3score");
 	labelTop3Score->setText("Hello Cocos!");
 	labelTop3Score->setFontName("Marker Felt");
 	labelTop3Score->setFontSize(30);
 	labelTop3Score->setColor(Color3B(159, 168, 176));
-	uiNode->addChild(labelTop3Score);
 
 	return true;
 }
@@ -108,5 +105,5 @@ void MenuScene::MuteGame(Ref *pSender, ui::TouchEventType type)
 
 void MenuScene::ExitGame(Ref *pSender, ui::TouchEventType type)
 {
-	
+	exit(0);
 }
